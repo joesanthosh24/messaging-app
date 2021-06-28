f6601795f1126e3d9295d24675ebf1d18b88a09d
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-06-28 10:20:31 -0400

    Added firebase config and did some cleanup to create a base application

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyC_di4QvE3swQD7npoNSlpoWX6bGY4pg9o",
+  authDomain: "messaging-app-28817.firebaseapp.com",
+  projectId: "messaging-app-28817",
+  storageBucket: "messaging-app-28817.appspot.com",
+  messagingSenderId: "756739902903",
+  appId: "1:756739902903:web:5ae36e5bad1f643f9aac32",
+};
