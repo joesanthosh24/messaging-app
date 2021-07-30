ef8a9c9a30e2e3715d89f5d1a451dae1a684edc8
joesanthosh15@gmail.com <joe.santhosh88@yahoo.com>
2021-07-30 16:06:27 -0400

    Added a loading screen for when authentication is in loading state before user is authenticated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
+import Spinner from "react-spinkit";
 
 import Header from "./components/header/header.component";
 import Sidebar from "./components/sidebar/sidebar.component";
 import Chat from "./components/chat/chat.component";
 import Login from "./components/login/login.component";
 
-import { AppBody } from "./App.styles";
+import { AppBody, AppLoading, AppLoadingContents } from "./App.styles";
 
 import "./App.css";
 
@@ -14,6 +15,20 @@ import { auth } from "./firebase";
 
 function App() {
   const [user, loading] = useAuthState(auth);
+
+  if (loading) {
+    return (
+      <AppLoading>
+        <AppLoadingContents>
+          <img
+            src="https://cdn.mos.cms.futurecdn.net/SDDw7CnuoUGax6x9mTo7dd.jpg"
+            alt="Logo"
+          />
+          <Spinner name="ball-spin-fade-loader" color="blue" fadeIn="none" />
+        </AppLoadingContents>
+      </AppLoading>
+    );
+  }
 
   return (
     <div className="app">
